2e8b7e190d080612193fab76dadb196e89e296c4
Saif-ul Hussain <saif-ulhussain@saif-uls-air.home>
2022-05-19 11:05:05 +0100

    Small change to Make Snippet exercise

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,14 +1,10 @@
 class MakeSnippet
-   
-   def make_snippet(text)
-       snippet = text.split(" ")
-        if snippet.length > 5
-         return "#{snippet[0..4].join(" ")}..."
-        else
-          return snippet.join(" ")
-        end
-   end
-    
+  def make_snippet(text)
+      snippet = text.split(" ")
+       if snippet.length > 5
+        return "#{snippet[0..4].join(" ")}..."
+       else
+         return snippet.join(" ")
+       end
+  end
 end
-
-
